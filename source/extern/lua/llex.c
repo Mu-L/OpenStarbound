c6769565bb58989d35e6c3ec2f3b2cbe2c17dbc2
goodusername123 <61405538+goodusername123@users.noreply.github.com>
2026-08-27 16:51:50 -0500

    Fix basic operators being broken (WHOOPS)
    pretty much speaks for itself..

--- source/extern/lua/llex.c
+++ source/extern/lua/llex.c
@@ -541,22 +541,22 @@ static int llex (LexState *ls, SemInfo *seminfo) {
       case '*': {   /* '*' or '*=' */
         next(ls);
         if (check_next1(ls, '=')) return TK_SCMULTIPLY;
-        else return '+';
+        else return '*';
       }
       case '%': {   /* '%' or '%=' */
         next(ls);
         if (check_next1(ls, '=')) return TK_SCMODULATE;
-        else return '+';
+        else return '%';
       }
       case '&': {   /* '&' or '&=' */
         next(ls);
         if (check_next1(ls, '=')) return TK_SCBITAND;
-        else return '+';
+        else return '&';
       }
       case '|': {   /* '|' or '|=' */
         next(ls);
         if (check_next1(ls, '=')) return TK_SCBITOR;
-        else return '+';
+        else return '|';
       }
       case '0': case '1': case '2': case '3': case '4':
       case '5': case '6': case '7': case '8': case '9': {
