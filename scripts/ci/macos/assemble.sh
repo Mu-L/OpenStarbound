085080f8be3cb6478a6cb8f6d60bb1ef97a00622
Bottinator22 <59987380+Bottinator22@users.noreply.github.com>
2026-08-30 15:23:42 -0700

    ...nevermind, turns out Mac is just weird. didn't read enough...

--- scripts/ci/macos/assemble.sh
+++ scripts/ci/macos/assemble.sh
@@ -15,14 +15,14 @@ mkdir client_distribution/osx/Starbound.app/Contents/MacOS
 cp dist/starbound client_distribution/osx/Starbound.app/Contents/MacOS/
 cp dist/*.dylib client_distribution/osx/Starbound.app/Contents/MacOS/
 cp \
-  dist/starbound \
+  dist/starbound_server \
   dist/btree_repacker \
   dist/asset_packer \
   dist/asset_unpacker \
   dist/dump_versioned_json \
   dist/make_versioned_json \
   scripts/ci/macos/sbinit.config \
-  scripts/ci/macos/run-client.sh \
+  scripts/ci/macos/run-server.sh \
   scripts/steam_appid.txt \
   client_distribution/osx/
 
